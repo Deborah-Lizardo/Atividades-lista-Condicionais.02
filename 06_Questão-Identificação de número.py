89c6ebadbc1d19b387e8250ab1472e45917e6a71
Deborah-Lizardo <deborah.slizardo@hotmail.com>
2024-03-20 10:38:40 -0300

    Create 06_Questão-Identificação de número.py

---
+++ 06_Questão-Identificação de número.py
@@ -0,0 +1,14 @@
+#Identificação de númeo pqar ou não;
+# quando for par,artrmazenar valor em P
+#Quando for ímpar,armazenar valor em I
+#Exibir valor final
+#Passo 1 : Valor ímpar ou par?
+#Passo 2 : valor vinculado;
+#Passo 3 : exibir valor final;
+numero = float(input("Digite o seu número: "))
+if numero %2 == 0 :
+    float(input(f"O número inserido ({numero:.0f})é par."))
+    numero = ("P")
+else :
+    float(input(f"O número inserido ({numero:.0f}) é ìmpar."))
+    numero = ("I")
